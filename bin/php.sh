94f5bc8e16d527ed75fada7c88a07bbe425ff237
robert.ahmerov <robert.ahmerov@lamoda.ru>
2017-11-01 22:14:12 +0300

    Clean code.

--- bin/php.sh
+++ bin/php.sh
@@ -1,52 +1,48 @@
 #!/usr/bin/env bash
 
+# Exit on error:
+set -e
+
 # Variables:
 IMAGE=${IMAGE:-$(basename $(pwd))}
 SERVER=${SERVER:-$IMAGE}
 PORT=${PORT:-9000}
-
-# Constant:
+DOCKER_MACHINE_NAME=${DOCKER_MACHINE_NAME:-default}
 PHP_FILE='./src/index.php'
 
-echo "Errors will stop this script."
-set -e
-
-echo "Build PHP image '$IMAGE' with xDebug (see ./docker/Dockerfile)."
-docker build --tag $IMAGE ./docker
-
-MACHINE_HOST_IP=$(
-    docker-machine \
-        ssh \
-        ${DOCKER_MACHINE_NAME:-default} \
-        "\
-            netstat -rn |\
-            grep ^0\.0\.0\.0 |\
-            awk '{{ print \$2 }}' \
-        " || echo ''
-)
-echo "Docker Machine Host IP: $MACHINE_HOST_IP"
-
+# IDE IP (host IP):
+GET_MACHINE_IP="netstat -rn | grep ^0\.0\.0\.0 | awk '{{ print \$2 }}'"
+MACHINE_HOST_IP=$(docker-machine ssh $DOCKER_MACHINE_NAME "$GET_MACHINE_IP"|| echo '')
 IDE_IP=${IDE_IP:-$MACHINE_HOST_IP}
-echo "Host IP (machine with IDE): $IDE_IP"
 [[ "$IDE_IP" = "" ]] && exit 1
 
+# Explicit xDebug host config:
+XDEBUG_CONFIG="remote_host=$IDE_IP"
+
+# Explicit server name for PhpStorm:
+PHP_IDE_CONFIG="serverName=$SERVER"
+
 cat <<X
-Running PHP-container with xDebug.
+Running PHP-image '$IMAGE' with xDebug.
 
 In your IDE:
 - Add breakpoint in '$PHP_FILE'.
 - Listen for debug on port $PORT.
 - Add server '$SERVER'.
-- Map $(pwd) -> /app.
+- Map local `$(pwd)` into VM path `/app`.
 X
 
+# Build image:
+docker build --tag $IMAGE ./docker
+
+# Run container:
 docker run \
     --rm \
     -it \
     --publish $PORT:9000 \
     --volume $(pwd):/app \
     --workdir /app \
-    --env XDEBUG_CONFIG="remote_host=$IDE_IP" \
-    --env PHP_IDE_CONFIG="serverName=$SERVER" \
+    --env XDEBUG_CONFIG="$XDEBUG_CONFIG" \
+    --env PHP_IDE_CONFIG="$PHP_IDE_CONFIG" \
     $IMAGE \
-        php $PHP_FILE
+    php $PHP_FILE
--- docker/build.sh
+++ docker/build.sh
@@ -3,14 +3,14 @@
 # Install xDebug:
 yes | pecl install xdebug
 
-# Add PHP config directory (goo.gl/QP3FRr):
-mkdir -p $PHP_INI_SCAN_DIR
-
 # Activate and configure xDebug:
-cat <<INI > $PHP_INI_SCAN_DIR/xdebug.ini
+cat <<INI >> /usr/local/etc/php/php.ini
+
+; https://xdebug.org/docs/
 [xdebug]
 zend_extension="$(php-config --extension-dir)/xdebug.so"
-xdebug.remote_enable=on
-xdebug.remote_autostart=on
-xdebug.remote_port=9000
+xdebug.remote_enable=on ; Connect to remote IDE.
+xdebug.remote_autostart=on ; Debug without GET / POST variable.
+xdebug.remote_port=9000 ; Default port.
+; xdebug.remote_connect_back=on ; This does not work, because of bridges.
 INI
